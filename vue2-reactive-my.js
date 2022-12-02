1d98f2edaba0768ca2a6e8e342a42e18a39b9031
tinfengyee <tinfengyee@163.com>
2022-12-03 02:11:12 +0800

    update

--- vue2-reactive-my.js
+++ vue2-reactive-my.js
@@ -1,12 +1,27 @@
-// 定义一个属性
-function def(obj, key, val, enumerable) {
+function defineReactive(obj, key, val) {
+  // value 可能是一个对象,要递归劫持，所以数据不能嵌套太深
+  observe(value);
+  const dep = new Dep();
   Object.defineProperty(obj, key, {
-    value: val,
-    enumerable: !!enumerable,
-    writable: true,
+    enumerable: true,
     configurable: true,
+    get: function reactiveGetter() {
+      // 如果有 watcher，就让 watcher 记住 dep，防止产生重复的 dep, 同时 dep 也收集此 watcher
+      if (Dep.target) {
+        dep.depend();
+      }
+      return val;
+    },
+    set: function reactiveSetter(newVal) {
+      // 数据没变动则不处理
+      if (val === newVal) return;
+      observe(newVal); // 如果新值是个对象，递归拦截
+      value = newVal; // 设置新的值
+      val = newVal;
+    },
   });
 }
+
 /**
  * 我们所讲的观察者/依赖其实就是 Watcher，我们要通知用到数据的地方，而使用这个数据的地方有很多，类型也不一样，有* 可能是组件的，有可能是用户写的 watch，我们就需要抽象出一个能集中处理这些情况的类。
  **/
@@ -46,28 +61,26 @@ class Watcher {
   }
 }
 
-function defineReactive(obj, key, val) {
-  // value 可能是一个对象,要递归劫持，所以数据不能嵌套太深
-  observe(value);
-  const dep = new Dep();
-  Object.defineProperty(obj, key, {
-    enumerable: true,
-    configurable: true,
-    get: function reactiveGetter() {
-      // 如果有 watcher，就让 watcher 记住 dep，防止产生重复的 dep, 同时 dep 也收集此 watcher
-      if (Dep.target) {
-        dep.depend();
-      }
-      return val;
-    },
-    set: function reactiveSetter(newVal) {
-      // 数据没变动则不处理
-      if (val === newVal) return;
-      observe(newVal); // 如果新值是个对象，递归拦截
-      value = newVal; // 设置新的值
-      val = newVal;
-    },
-  });
+class Dep {
+  constructor() {
+    this.id = uid++;
+    this.subs = [];
+  }
+  addSub(sub) {
+    this.subs.push(sub);
+  }
+  removeSub(sub) {
+    // 省略
+  }
+  depend() {
+    if (Dep.target) {
+      // 让正在执行的watcher记录dep，同时dep也会记录watcher
+      Dep.target.addDep(this);
+    }
+  }
+  notify() {
+    this.subs.forEach((watcher) => watcher.update());
+  }
 }
 
 class Observer {
@@ -97,24 +110,12 @@ function observe(value) {
   return new Observer(value);
 }
 
-class Dep {
-  constructor() {
-    this.id = uid++;
-    this.subs = [];
-  }
-  addSub(sub) {
-    this.subs.push(sub);
-  }
-  removeSub(sub) {
-    // 省略
-  }
-  depend() {
-    if (Dep.target) {
-      // 让正在执行的watcher记录dep，同时dep也会记录watcher
-      Dep.target.addDep(this);
-    }
-  }
-  notify() {
-    this.subs.forEach((watcher) => watcher.update());
-  }
+// 定义一个属性
+function def(obj, key, val, enumerable) {
+  Object.defineProperty(obj, key, {
+    value: val,
+    enumerable: !!enumerable,
+    writable: true,
+    configurable: true,
+  });
 }
